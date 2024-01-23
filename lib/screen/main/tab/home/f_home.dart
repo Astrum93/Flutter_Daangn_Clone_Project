fce5356a02d11a3e0038d6ff025ec8613e1bef88
Astrum93 <jungmin2670@gmail.com>
2024-01-23 23:28:47 +0900

    240123 riverpod을 사용하여 ScrollController 추가 및 Initstate

--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -1,9 +1,30 @@
+import 'package:fast_app_base/screen/main/fab/w_floating_daangn_button.riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeFragment extends StatelessWidget {
-  const HomeFragment({
-    Key? key,
-  }) : super(key: key);
+class HomeFragment extends ConsumerStatefulWidget {
+  const HomeFragment({super.key});
+
+  @override
+  ConsumerState<HomeFragment> createState() => _HomeFragmentState();
+}
+
+class _HomeFragmentState extends ConsumerState<HomeFragment> {
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    scrollController.addListener(() {
+      final isSmall = ref.read(floatingButtonIsSmallProvider);
+
+      if (scrollController.position.pixels > 100 && !isSmall) {
+        ref.read(floatingButtonIsSmallProvider.notifier).state = true;
+      } else if (scrollController.position.pixels < 100 && isSmall) {
+        ref.read(floatingButtonIsSmallProvider.notifier).state = false;
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
