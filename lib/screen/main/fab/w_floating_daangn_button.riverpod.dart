b51499a560fca74ca932165ccdc6ed905067809d
Astrum93 <jungmin2670@gmail.com>
2024-01-23 23:44:05 +0900

    240123 State를 같이 관리하기 위한 새로운 Class와 Notifier, Provider 생성

--- lib/screen/main/fab/w_floating_daangn_button.riverpod.dart
+++ lib/screen/main/fab/w_floating_daangn_button.riverpod.dart
@@ -1,4 +1,28 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final floatingButtonIsExpandedProvider = StateProvider((ref) => false);
-final floatingButtonIsSmallProvider = StateProvider((ref) => false);
+/// State를 같이 관리하고 싶은 경우
+class FloatingButtonState {
+  bool isExpanded;
+  bool isSmall;
+
+  FloatingButtonState()
+      : isExpanded = false,
+        isSmall = false;
+}
+
+/// State와 Notifier를 가진 Provider
+final floatingButtonStateProvider =
+    StateNotifierProvider<FloatingButtonStateNotifier, FloatingButtonState>(
+  (ref) => FloatingButtonStateNotifier(
+    FloatingButtonState(),
+  ),
+);
+
+/// State를 감지하는 Notifier
+class FloatingButtonStateNotifier extends StateNotifier<FloatingButtonState> {
+  FloatingButtonStateNotifier(super.state);
+}
+
+/// 개별로 State를 관리하고 싶은 경우
+// final floatingButtonIsExpandedProvider = StateProvider((ref) => false);
+// final floatingButtonIsSmallProvider = StateProvider((ref) => false);
