fb0e022ff0efa6a0e0f075328f0a64923aeb94cc
Astrum93 <jungmin2670@gmail.com>
2024-02-05 22:46:45 +0900

    240205 FloatingDaangnButton에 height 값을 추가하여 Home 화면의 padding 값으로 사용.

--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -1,5 +1,6 @@
 import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/entity/dummies.dart';
+import 'package:fast_app_base/screen/main/fab/w_floating_daangn_button.dart';
 import 'package:fast_app_base/screen/main/fab/w_floating_daangn_button.riverpod.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_product_post_item.dart';
 import 'package:flutter/material.dart';
@@ -53,6 +54,7 @@ class _HomeFragmentState extends ConsumerState<HomeFragment> {
         ),
         Expanded(
           child: ListView.separated(
+            padding: const EdgeInsets.only(bottom: FloatingDaangnButton.height),
             controller: scrollController,
             itemBuilder: (context, index) => ProductPostItem(postList[index]),
             itemCount: postList.length,
--- lib/screen/main/tab/home/w_product_post_item.dart
+++ lib/screen/main/tab/home/w_product_post_item.dart
@@ -11,41 +11,62 @@ class ProductPostItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Stack(
       children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: CachedNetworkImage(
-            imageUrl: post.product.images[0],
-            width: 150,
-          ),
-        ),
-        const Width(10),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              post.content.text.size(17).bold.make(),
-              Row(
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: CachedNetworkImage(
+                imageUrl: post.product.images[0],
+                width: 150,
+              ),
+            ),
+            const Width(10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  post.address.simpleAddress.text
-                      .color(context.appColors.lessImportantColor)
-                      .make(),
-                  '•'.text.color(context.appColors.lessImportantColor).make(),
-                  timeago
-                      .format(post.createdTime,
-                          locale: context.locale.languageCode)
-                      .text
-                      .color(context.appColors.lessImportantColor)
-                      .make()
+                  post.content.text.size(17).bold.make(),
+                  Row(
+                    children: [
+                      post.address.simpleAddress.text
+                          .color(context.appColors.lessImportantColor)
+                          .make(),
+                      '•'
+                          .text
+                          .color(context.appColors.lessImportantColor)
+                          .make(),
+                      timeago
+                          .format(post.createdTime,
+                              locale: context.locale.languageCode)
+                          .text
+                          .color(context.appColors.lessImportantColor)
+                          .make()
+                    ],
+                  ),
+                  post.product.price.toWon().text.bold.make(),
                 ],
               ),
-              post.product.price.toWon().text.bold.make(),
-            ],
+            ),
+          ],
+        ).p(15),
+        Positioned.fill(
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset('$basePath/home/post_chat_count.png'),
+                post.chatCount.text.make(),
+                Image.asset('$basePath/home/post_heart_off.png'),
+                post.likeCount.text.make(),
+              ],
+            ),
           ),
         )
       ],
-    ).p(15);
+    );
   }
 }
