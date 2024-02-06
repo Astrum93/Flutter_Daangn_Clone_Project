fba4d142f3b8921e88bfbc8b3823fd43439b5779
Astrum93 <jungmin2670@gmail.com>
2024-02-07 00:38:09 +0900

    240207 notification provider를 watch했을 때 값이 null인 경우 CircularProgressIndicator

--- lib/screen/notification/f_notification_list.dart
+++ lib/screen/notification/f_notification_list.dart
@@ -1,3 +1,6 @@
+import 'package:fast_app_base/screen/notification/provider/notification_provider.dart';
+import 'package:fast_app_base/screen/notification/vo/notification%20_dummies.dart';
+import 'package:fast_app_base/screen/notification/w_notification_item.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -6,6 +9,16 @@ class NotificationFragment extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Placeholder();
+    final list = ref.watch(notificationProvider);
+    return list == null
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : ListView(
+            children: notificationList
+                .map((e) =>
+                    NotificationItemWidget(onTap: () {}, notification: e))
+                .toList(),
+          );
   }
 }
