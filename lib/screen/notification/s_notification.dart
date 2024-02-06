2fca954fc09a9bb7252550926901ec6abb797ca3
Astrum93 <jungmin2670@gmail.com>
2024-02-06 23:38:52 +0900

    240206 알림 화면 AppBar 만들기

--- lib/screen/notification/s_notification.dart
+++ lib/screen/notification/s_notification.dart
@@ -1,3 +1,4 @@
+import 'package:fast_app_base/common/common.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,6 +9,28 @@ class NotificationScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Placeholder();
+    final isEditMode = ref.watch(notificationEditModeProvider);
+    return Column(
+      children: [
+        AppBar(
+          title: '알림'.text.make(),
+          actions: [
+            Tap(
+              onTap: () {
+                ref.read(notificationEditModeProvider.notifier).state =
+                    !isEditMode;
+              },
+              child: (isEditMode ? '완료' : '편집').text.make().p(10),
+            ),
+          ],
+        ),
+        TabBarView(
+          children: [
+            '활동 알림'.text.make(),
+            '키워드 알림'.text.make(),
+          ],
+        ),
+      ],
+    );
   }
 }
