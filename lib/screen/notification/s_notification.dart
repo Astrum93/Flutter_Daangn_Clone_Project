338df653c5d714888e4003693dcf3d5881ccc9b2
Astrum93 <jungmin2670@gmail.com>
2024-02-06 23:42:10 +0900

    240206 HookConsumerWidget으로 변경 후 tabController 생성

--- lib/screen/notification/s_notification.dart
+++ lib/screen/notification/s_notification.dart
@@ -1,14 +1,16 @@
 import 'package:fast_app_base/common/common.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final notificationEditModeProvider = StateProvider((ref) => false);
 
-class NotificationScreen extends ConsumerWidget {
+class NotificationScreen extends HookConsumerWidget {
   const NotificationScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final tabController = useTabController(initialLength: 2);
     final isEditMode = ref.watch(notificationEditModeProvider);
     return Column(
       children: [
