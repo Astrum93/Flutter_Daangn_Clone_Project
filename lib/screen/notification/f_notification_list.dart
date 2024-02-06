4178b317f955bcf40060942588a275b5dc93bdf8
Astrum93 <jungmin2670@gmail.com>
2024-02-07 00:02:05 +0900

    240206 NotificationFragment 생성

---
+++ lib/screen/notification/f_notification_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class NotificationFragment extends HookConsumerWidget {
+  const NotificationFragment({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Placeholder();
+  }
+}
