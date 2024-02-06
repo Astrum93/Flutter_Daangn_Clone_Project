b5e8c2e62906dbf9a17bff12403384b014c4fc8f
Astrum93 <jungmin2670@gmail.com>
2024-02-07 00:24:01 +0900

    240207 notification Provider 생성

---
+++ lib/screen/notification/provider/notification_provider.dart
@@ -0,0 +1,6 @@
+import 'package:fast_app_base/screen/notification/vo/vo_notification.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final remoteNotificationProvider = FutureProvider((ref) => );
+
+final notificationProvider = StateProvider<List<DaangnNotification>>((ref) => null)
