182a087861a70d8100c039e542bd218ba139984d
Astrum93 <jungmin2670@gmail.com>
2024-02-08 20:10:17 +0900

    240208 PostDetailScreen에서 사용 할 _PostDetail 위젯 생성

--- lib/screen/post_detail/s_post_detail.dart
+++ lib/screen/post_detail/s_post_detail.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/entity/post/vo_product_post.dart';
 import 'package:fast_app_base/entity/post/vo_simple_product_post.dart';
 import 'package:fast_app_base/screen/post_detail/provider/product_post_provider.dart';
 import 'package:flutter/material.dart';
@@ -27,5 +28,17 @@ class PostDetailScreen extends ConsumerWidget {
         child: CircularProgressIndicator(),
       ),
     );
+  }
+}
+
+class _PostDetail extends StatelessWidget {
+  final SimpleProductPost simpleProductPost;
+  final ProductPost? productPost;
+
+  const _PostDetail(this.simpleProductPost, {super.key, this.productPost});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
