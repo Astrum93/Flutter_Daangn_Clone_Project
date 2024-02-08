fbff9a2b5d967f3d93c4ce4cbc629da26063cd7f
Astrum93 <jungmin2670@gmail.com>
2024-02-09 04:08:13 +0900

    240209 PostDetail의 UI 완료

--- lib/screen/post_detail/s_post_detail.dart
+++ lib/screen/post_detail/s_post_detail.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/common/widget/w_round_button.dart';
 import 'package:fast_app_base/entity/post/vo_product_post.dart';
 import 'package:fast_app_base/entity/post/vo_simple_product_post.dart';
 import 'package:fast_app_base/screen/post_detail/provider/product_post_provider.dart';
@@ -9,6 +10,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+import '../../entity/product/vo_product.dart';
 
 class PostDetailScreen extends ConsumerWidget {
   final SimpleProductPost? simpleProductPost;
@@ -25,7 +28,7 @@ class PostDetailScreen extends ConsumerWidget {
     final productPost = ref.watch(productPostProvider(id));
 
     return productPost.when(
-      data: (data) => _PostDetail(data.simpleProductPost),
+      data: (data) => _PostDetail(data.simpleProductPost, productPost: data),
       error: (error, trace) => '에러발생'.text.make(),
       loading: () => simpleProductPost != null
           ? _PostDetail(simpleProductPost!)
@@ -39,8 +42,9 @@ class PostDetailScreen extends ConsumerWidget {
 class _PostDetail extends HookWidget {
   final SimpleProductPost simpleProductPost;
   final ProductPost? productPost;
+  static const bottomMenuHeight = 100.0;
 
-  const _PostDetail(this.simpleProductPost, {super.key, this.productPost});
+  const _PostDetail(this.simpleProductPost, {this.productPost});
 
   @override
   Widget build(BuildContext context) {
@@ -50,11 +54,14 @@ class _PostDetail extends HookWidget {
         children: [
           Positioned.fill(
             child: SingleChildScrollView(
+              padding: const EdgeInsets.only(bottom: bottomMenuHeight),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   _ImagePager(
-                      pageController: pageController,
-                      simpleProductPost: simpleProductPost),
+                    pageController: pageController,
+                    simpleProductPost: simpleProductPost,
+                  ),
                   UserProfileWidget(
                     simpleProductPost.product.user,
                     address: simpleProductPost.address,
@@ -69,11 +76,60 @@ class _PostDetail extends HookWidget {
           const _AppBar(),
           Align(
             alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 100,
-              color: Colors.blue,
-            ),
+            child: PostDetailBottomMenu(simpleProductPost.product),
           )
+        ],
+      ),
+    );
+  }
+}
+
+class PostDetailBottomMenu extends StatelessWidget {
+  final Product product;
+
+  const PostDetailBottomMenu(
+    this.product, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: _PostDetail.bottomMenuHeight,
+      child: Column(
+        children: [
+          const Line(),
+          Expanded(
+            child: Row(
+              children: [
+                Image.asset('$basePath/detail/heart_on.png', height: 25),
+                width30,
+                const VerticalDivider().pSymmetric(v: 15),
+                width30,
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          product.price.toWon().text.bold.make(),
+                        ],
+                      ),
+                      '가격 제안하기'.text.orange400.underline.make(),
+                    ],
+                  ),
+                ),
+                RoundButton(
+                  text: '채팅하기',
+                  onTap: () {},
+                  bgColor: Colors.orange,
+                  borderRadius: 7,
+                ),
+                width10,
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -82,7 +138,6 @@ class _PostDetail extends HookWidget {
 
 class _ImagePager extends StatelessWidget {
   const _ImagePager({
-    super.key,
     required this.pageController,
     required this.simpleProductPost,
   });
@@ -128,9 +183,7 @@ class _ImagePager extends StatelessWidget {
 }
 
 class _AppBar extends StatelessWidget {
-  const _AppBar({
-    super.key,
-  });
+  const _AppBar();
 
   @override
   Widget build(BuildContext context) {
