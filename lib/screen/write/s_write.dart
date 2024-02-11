e30ce245d434e6ea19eec87e61baf5da1ebc5f78
Astrum93 <jungmin2670@gmail.com>
2024-02-11 22:43:12 +0900

    240211 각각의 Widget 영역 구분

--- lib/screen/write/s_write.dart
+++ lib/screen/write/s_write.dart
@@ -1,3 +1,5 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/common/widget/w_round_button.dart';
 import 'package:flutter/material.dart';
 
 class WriteScreen extends StatefulWidget {
@@ -8,8 +10,46 @@ class WriteScreen extends StatefulWidget {
 }
 
 class _WriteScreenState extends State<WriteScreen> {
+  final List<String> imageList = [];
+
+  final titleController = TextEditingController();
+  final priceController = TextEditingController();
+  final descriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: '내 물건 팔기'.text.bold.make(),
+        actions: [
+          Tap(
+            onTap: () {},
+            child: '임시저장'.text.make().p(15),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _ImageSelectWidget(imageList),
+            _TitleEditor(titleController),
+            _PriceEditor(priceController),
+            _DescEditor(descriptionController),
+          ],
+        ),
+      ),
+      bottomSheet: RoundButton(
+        text: '작성 완료',
+        onTap: () {},
+      ),
+    );
   }
 }
+
+class _ImageSelectWidget {}
+
+class _TitleEditor {}
+
+class _PriceEditor {}
+
+class _DescEditor {}
