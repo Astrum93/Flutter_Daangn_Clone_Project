1c99830bc69879ffb8f1212c526b4b2ed8fff308
Astrum93 <jungmin2670@gmail.com>
2024-02-13 23:16:19 +0900

    240213 Image 추가, 삭제 구현 완료.

--- lib/screen/write/s_write.dart
+++ lib/screen/write/s_write.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/util/app_keyboard_util.dart';
 import 'package:fast_app_base/common/widget/round_button_theme.dart';
@@ -12,6 +15,7 @@ import 'package:fast_app_base/screen/post_detail/s_post_detail.dart';
 import 'package:fast_app_base/screen/write/d_select_image_source.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 
 class WriteScreen extends ConsumerStatefulWidget {
   const WriteScreen({super.key});
@@ -63,9 +67,27 @@ class _WriteScreenState extends ConsumerState<WriteScreen>
           children: [
             _ImageSelectWidget(
               imageList,
+              onTapDeleteImage: (imagePath) {
+                setState(() {
+                  imageList.remove(imagePath);
+                });
+              },
               onTap: () async {
                 /// 옵션 Dialog
                 final selectedSource = await SelectImageSourceDialog().show();
+
+                if (selectedSource == null) {
+                  return;
+                }
+
+                final file =
+                    await ImagePicker().pickImage(source: selectedSource);
+                if (file == null) {
+                  return;
+                }
+                setState(() {
+                  imageList.add(file.path);
+                });
               },
             ),
             _TitleEditor(titleController),
@@ -132,43 +154,89 @@ class _WriteScreenState extends ConsumerState<WriteScreen>
 class _ImageSelectWidget extends StatelessWidget {
   final List<String> imageList;
   final VoidCallback onTap;
+  final void Function(String path) onTapDeleteImage;
 
-  const _ImageSelectWidget(this.imageList, {required this.onTap});
+  const _ImageSelectWidget(this.imageList,
+      {required this.onTap, required this.onTapDeleteImage});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            SelectImageButton(onTap: onTap, imageList: imageList)
+                .pOnly(top: 10, right: 4),
+            ...imageList.map(
+              (imagePath) => Stack(
+                children: [
+                  SizedBox(
+                    width: 80,
+                    height: 80,
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Image.file(File(imagePath), fit: BoxFit.fill)),
+                  ).pOnly(left: 4, right: 10, top: 10),
+                  Positioned.fill(
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: Tap(
+                        onTap: () {
+                          onTapDeleteImage(imagePath);
+                        },
+                        child: Transform.rotate(
+                          angle: pi / 4,
+                          child: const Icon(Icons.add_circle),
+                        ).pOnly(left: 30, bottom: 30),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SelectImageButton extends StatelessWidget {
+  const SelectImageButton({
+    super.key,
+    required this.onTap,
+    required this.imageList,
+  });
+
+  final VoidCallback onTap;
+  final List<String> imageList;
 
   @override
   Widget build(BuildContext context) {
     return Tap(
       onTap: onTap,
       child: SizedBox(
-        height: 100,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              SizedBox(
-                width: 80,
-                height: 80,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(Icons.camera_alt),
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: imageList.length.toString(),
-                            style: const TextStyle(color: Colors.orange),
-                          ),
-                          const TextSpan(text: '/10')
-                        ],
-                      ),
-                    ),
-                  ],
-                ).box.rounded.border(color: Colors.grey).make(),
+        width: 80,
+        height: 80,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.camera_alt),
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: imageList.length.toString(),
+                    style: const TextStyle(color: Colors.orange),
+                  ),
+                  const TextSpan(text: '/10')
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          ],
+        ).box.rounded.border(color: Colors.grey).make(),
       ),
     );
   }
