980be13d799aa54a34286e0ccd202fe682aeada2
Astrum93 <jungmin2670@gmail.com>
2024-02-17 21:25:12 +0900

    240217 상세페이지 이동 Go Router로 변경

--- lib/screen/main/tab/home/w_product_post_item.dart
+++ lib/screen/main/tab/home/w_product_post_item.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/entity/post/vo_simple_product_post.dart';
-import 'package:fast_app_base/screen/post_detail/s_post_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
@@ -15,8 +14,11 @@ class ProductPostItem extends StatelessWidget {
     return Tap(
       onTap: () {
         // 상세 페이지 이동
-        Nav.push(PostDetailScreen(post.id, simpleProductPost: post),
-            durationMs: 800, navAni: NavAni.Right);
+        context.go('/main/localLife/${post.id}');
+
+        // 상세 페이지 이동2
+        // Nav.push(PostDetailScreen(post.id, simpleProductPost: post),
+        //     durationMs: 800, navAni: NavAni.Right);
       },
       child: Stack(
         children: [
