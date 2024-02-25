db9627e3b27044345c8d37f61fb3977fc7585476
Astrum93 <jungmin2670@gmail.com>
2024-02-25 21:15:32 +0900

    240225 AutoDispose Extention 만들기

--- lib/screen/post_detail/provider/product_post_provider.dart
+++ lib/screen/post_detail/provider/product_post_provider.dart
@@ -1,3 +1,5 @@
+import 'package:fast_app_base/common/cli_common.dart';
+import 'package:fast_app_base/common/dart/extension/ref_extension.dart';
 import 'package:fast_app_base/entity/post/vo_product_post.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -5,5 +7,6 @@ import '../../../data/network/daangn_api.dart';
 
 final productPostProvider =
     AutoDisposeFutureProviderFamily<ProductPost, int>((ref, id) async {
+  ref.cacheFor(10.minutes);
   return await DaangnApi.getPost(id);
 });
