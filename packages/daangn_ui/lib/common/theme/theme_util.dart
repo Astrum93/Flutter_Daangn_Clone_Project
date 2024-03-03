7a8422529e7532f0fdea40dd21a6038637d8f945
Astrum93 <jungmin2670@gmail.com>
2024-03-03 22:13:57 +0900

    240303 daangn_ui 패키지 추가에 따른 에러 해결 (3)

--- packages/daangn_ui/lib/common/theme/theme_util.dart
+++ packages/daangn_ui/lib/common/theme/theme_util.dart
@@ -1,7 +1,15 @@
+import 'package:daangn_ui/common/common.dart';
+import 'package:daangn_ui/common/theme/custom_theme.dart';
+import 'package:daangn_ui/common/theme/custom_theme_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
 class ThemeUtil {
   static Brightness get systemBrightness =>
       SchedulerBinding.instance.platformDispatcher.platformBrightness;
+
+  static void changeTheme(BuildContext context, CustomTheme theme) {
+    CustomThemeApp.saveThemeFunction(theme);
+    context.changeTheme(theme);
+  }
 }
